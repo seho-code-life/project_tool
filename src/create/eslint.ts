9a71c984fe3cbd01f8b3e6df3029f5920d0edcac
seho <1018715564@qq.com>
2021-09-17 14:31:34 +0800

    🐞 fix: 修复eslint的创建依赖文件的逻辑

--- src/create/eslint.ts
+++ src/create/eslint.ts
@@ -2,14 +2,14 @@
 
 import fs from 'fs';
 import path from 'path';
+import { exists } from './../util/file';
 
 const main = (): void => {
   // 将template中的vscode内容拷贝到根目录
   const rc = fs.readFileSync(path.resolve(__dirname, '..', 'template/eslint/.eslintrc.js'));
-  console.log(rc);
   const ignore = fs.readFileSync(path.resolve(__dirname, '..', 'template/eslint/.eslintignore'));
-  // fs.writeFileSync('', rc);
-  // fs.writeFileSync('', ignore);
+  fs.writeFileSync('.eslintrc.js', rc);
+  fs.writeFileSync('.eslintignore', ignore);
 };
 
 export default main;
